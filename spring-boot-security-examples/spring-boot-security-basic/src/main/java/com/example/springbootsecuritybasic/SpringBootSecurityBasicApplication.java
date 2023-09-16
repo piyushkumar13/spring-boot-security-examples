8f294bbf4f45a839ba0416149d21088b688da122
piyushkumar13 <piyush.k9013@gmail.com>
2023-09-16 17:44:56 +0530

    adding modules

--- spring-boot-security-examples/spring-boot-security-basic/src/main/java/com/example/springbootsecuritybasic/SpringBootSecurityBasicApplication.java
+++ spring-boot-security-examples/spring-boot-security-basic/src/main/java/com/example/springbootsecuritybasic/SpringBootSecurityBasicApplication.java
@@ -1,13 +1,13 @@
-package com.example.springbootsecurityexamples;
+package com.example.springbootsecuritybasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSecurityExamplesApplication {
+public class SpringBootSecurityBasicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootSecurityExamplesApplication.class, args);
+        SpringApplication.run(SpringBootSecurityBasicApplication.class, args);
     }
 
 }
---
+++ spring-boot-security-examples/spring-boot-security-form/src/main/java/com/example/springbootsecurityform/SpringBootSecurityFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecurityform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityFormApplication.class, args);
+    }
+
+}
